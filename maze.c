03e210269db99a8c0698a4814e936025fec6169f
DeviMancer <allucardo24@gmail.com>
2024-11-03 10:34:25 -0700

    Print Minotaur in the center of the maze

--- maze.c
+++ maze.c
@@ -6,12 +6,13 @@
 void maze_print()
 {
     //Supongamos que queremos un laberinto de 5x5
-    size_t x = 5;     
-    size_t y = 5;
+    size_t x = 9;     
+    size_t y = 9;
 
-    // Usamos ncurses.
+    //***** Usamos ncurses.
     initscr();
-    
+    curs_set(0);
+
     WINDOW * win = newwin(y*2 + 1, x*4 + 1, 0, 0);
     refresh();
 
@@ -29,15 +30,28 @@ void maze_print()
     for(size_t j = 0; j < 2*x + 1; ++j) wprintw(win, "+ "); // ** Imprime linea temporal divisora.
     //wprintw(win, "\n\n");
 
-    // Crea una caja donde el borde esta formado por el caracter c.
+
+    //***** Crea una caja donde el borde esta formado por el caracter c.
+    //TO-DO: Que el borde top y bottom tengan coherencia con la estetica.
     char c = '+';
 
     box(win, 0, 0);
     wborder(win, (int)c, (int)c, (int)c, (int)c, (int)c, (int)c, (int)c, (int)c);
     wrefresh(win);
 
-    //TO-DO: Que el borde top y bottom tengan coherencia con la estetica.
-    //
+    
+    //***** Imprime el minotauro en el centro.
+    size_t maxX, maxY, offSetX, offSetY;
+    getmaxyx(win, maxY, maxX);
+
+    // Desplazamos el minotauro si las dimensiones del laberinto son par.
+    if(x % 2 == 0) offSetX = -2; // Desplazamiento de 2 columnas a la derecha.  
+    if(y % 2 == 0) offSetY = -1; // Desplazamiento de 1 renglon hacia arriba.
+
+    mvwprintw(win, maxY/2 + offSetY, maxX/2 + offSetX, "M"); // Imprime el minotauro.
+    wrefresh(win);
+
+
     getch();
     endwin();
 }
